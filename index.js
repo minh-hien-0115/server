7b7e981329d9a968e1b6c0b32b3e0ba022fbcaab
Minh Hien <minh.hien.0115@gmail.com>
2025-05-05 10:10:22 +0700

    create server

---
+++ index.js
@@ -0,0 +1,20 @@
+// const e = require('cors');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/auth/hello', (_req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Server starting at http://localhost:${PORT}`);
+  }
+})
