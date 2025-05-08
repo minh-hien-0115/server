6abe25cdb5c27997adfedfba351ec0f5173b8932
Minh Hien <minh.hien.0115@gmail.com>
2025-05-08 09:01:41 +0700

    finish verification

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,11 +24,77 @@ const getJsonWebToken = async (email, id) => {
     return token;
 }
 
+const verificationCode = Math.round(1000 + Math.random() * 9000)
 const handleSendMail = async (val, email) => {
-    const verificationCode = Math.round(1000 + Math.random() * 9000)
+    // const verificationCode = Math.round(1000 + Math.random() * 9000)
+    // try {
+    //     await transporter.sendMail({
+        //     from: `AppChat <${process.env.USERNAME_EMAIL}>`,
+        //     to: email,
+        //     subject: "Yêu cầu xác thực",
+        //     html: `
+        //     <div style="max-width:600px;margin:0 auto;padding:20px;font-family:sans-serif;border:1px solid #ddd;border-radius:10px;">
+        //       <h2 style="color:#4A90E2;">Mã xác nhận AppChat</h2>
+        //       <p>Chào bạn,</p>
+        //       <p>Chúng tôi đã nhận được yêu cầu xác thực tài khoản của bạn. Mã xác nhận này có thể được sử dụng để xác thực tài khoản của bạn.</p>
+        //       <p>Vui lòng sử dụng mã bên dưới để hoàn tất đăng ký:</p>
+        //       <div style="font-size:28px; font-weight:bold; margin:20px 0; padding:10px; background:#f0f0f0; border-radius:8px; text-align:center; letter-spacing:4px;">
+        //         ${verificationCode}
+        //       </div>
+        //       <p>Mã xác thực này chỉ có hiệu lực trong 60 giây.</p>
+        //       <p>Nếu bạn không yêu cầu, vui lòng bỏ qua email này.</p>
+        //       <hr />
+        //       <p style="font-size:12px;color:#888;">AppChat - Ứng dụng nhắn tin bảo mật</p>
+        //     </div>
+        //   `,
+    //     });
+
+    //     console.log("Email đã gửi đến:", email, "với mã:", verificationCode);
+    //     // return verificationCode;
+
+    //     // res.status(200).json({
+    //     //     message: "Email đã được gửi thành công!!!",
+    //     //     data: {
+    //     //         code: verificationCode
+    //     //     }
+    //     // })
+
+    //     return 'OK'
+
+    // } catch (error) {
+    //     // console.error(`Không thể gửi Email: ${error}`);
+    //     // res.status(401)
+    //     // throw new Error(`Gửi email thất bại`);
+    //     return error;
+    // }
+
+    // try {
+    //     await transporter.sendMail({
+    //       from: `Hỗ trợ Application AppChat <${process.env.USERNAME_EMAIL}>`,
+    //       to: email,
+    //       subject: "Mã xác thực Email của bạn",
+    //       text: "",
+    //       html: `<b>Mã xác thực của bạn là: ${verificationCode}</b>`,
+    //     });
+    // } catch (error) {
+    //     console.log("Không thể gửi Email")
+    // }
+
     try {
-        await transporter.sendMail({
-            from: `AppChat <${process.env.USERNAME_EMAIL}>`,
+        await transporter.sendMail(val)
+        return 'OK'
+    } catch (error) {
+        return error;
+    }
+}
+
+const verification = asyncHandle(async (req, res) => {
+    const {email} = req.body;
+    const verificationCode = Math.round(1000 + Math.random() * 9000)
+
+    try {
+        const data = {
+			from: `AppChat <${process.env.USERNAME_EMAIL}>`,
             to: email,
             subject: "Yêu cầu xác thực",
             html: `
@@ -46,56 +112,37 @@ const handleSendMail = async (val, email) => {
               <p style="font-size:12px;color:#888;">AppChat - Ứng dụng nhắn tin bảo mật</p>
             </div>
           `,
-        });
+		};
 
-        // console.log("Email đã gửi đến:", email, "với mã:", verificationCode);
-        // return verificationCode;
+		await handleSendMail(data);
 
-        // res.status(200).json({
-        //     message: "Email đã được gửi thành công!!!",
-        //     data: {
-        //         code: verificationCode
-        //     }
-        // })
-
-        return 'OK'
-
+		res.status(200).json({
+			message: 'Send verification code successfully!!!',
+			data: {
+				code: verificationCode,
+			},
+		});
     } catch (error) {
-        // console.error(`Không thể gửi Email: ${error}`);
-        // res.status(401)
-        // throw new Error(`Gửi email thất bại`);
-        return error;
+        res.status(401);
+		throw new Error('Can not send email');
     }
 
-    // try {
-    //     await transporter.sendMail({
-    //       from: `Hỗ trợ Application AppChat <${process.env.USERNAME_EMAIL}>`,
-    //       to: email,
-    //       subject: "Mã xác thực Email của bạn",
-    //       text: "",
-    //       html: `<b>Mã xác thực của bạn là: ${verificationCode}</b>`,
-    //     });
-    // } catch (error) {
-    //     console.log("Không thể gửi Email")
-    // }
-}
-
-const verification = asyncHandle(async (req, res) => {
-    const { email } = req.body;
-    const verificationCode = Math.round(1000 + Math.random() * 9000)
+    // const { email } = req.body;
+    // // const verificationCode = Math.round(1000 + Math.random() * 9000)
     
-    try {
-        await handleSendMail(verificationCode, email)
-        res.status(200).json({
-            message: "Email đã được gửi thành công!!!",
-            data: {
-                code: verificationCode
-            }
-        })
-    } catch (error) {
-        res.status(401)
-        throw new Error(`Gửi email thất bại`);
-    }
+    // try {
+    //     await handleSendMail(verificationCode, email)
+    //     res.status(200).json({
+    //         message: "Email đã được gửi thành công!!!",
+    //         data: {
+    //             code: verificationCode
+    //         }
+    //     })
+    //     console.log("Email gửi đến:", email, "với mã:", verificationCode);
+    // } catch (error) {
+    //     res.status(401)
+    //     throw new Error(`Gửi email thất bại`);
+    // }
 })
 
 const register = asyncHandle(async (req, res) => {
